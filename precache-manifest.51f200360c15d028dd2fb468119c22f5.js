5ca66e7390f723e820c490baae823ade3da7a2dd
unnome <unnome@gmail.com>
2019-10-27 20:47:18 +0100

    Updates

---
+++ precache-manifest.51f200360c15d028dd2fb468119c22f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e180054df12d20e46c3142d9de2ee1",
+    "url": "/unnome.github.io/attica/index.html"
+  },
+  {
+    "revision": "9ec28f6016a703dc8dcd",
+    "url": "/unnome.github.io/attica/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "60e8631f82b8eda13f54",
+    "url": "/unnome.github.io/attica/static/js/2.3681252e.chunk.js"
+  },
+  {
+    "revision": "9ec28f6016a703dc8dcd",
+    "url": "/unnome.github.io/attica/static/js/main.172c7a97.chunk.js"
+  },
+  {
+    "revision": "91086e23474c9fb47b5a",
+    "url": "/unnome.github.io/attica/static/js/runtime-main.2715f3d2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/unnome.github.io/attica/static/media/logo.25bf045c.svg"
+  }
+]);
